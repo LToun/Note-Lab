301596eba99ebae491b06fd83ab70088ee9141e7
AntonioLaNa <38198540+AntonioLaNa@users.noreply.github.com>
2021-07-25 01:02:50 -0500

    Initial Commit

---
+++ NoteLab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoteLab
+//
+//  Created by Antonio Lara Navarrete on 25/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
